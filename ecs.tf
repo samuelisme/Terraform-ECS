b2be19857cd04f2bc31e1b925f659e136596c7fc
Samuel Chan <samuel.chan@compareglobalgroup.com>
2023-01-02 20:42:57 +0000

    update Terraform

--- ecs.tf
+++ ecs.tf
@@ -1,1 +1,261 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+  backend "s3" {
+        bucket = "ecsbucket" //S3 bucket to store Terraform state file
+        key    = "state.tfstate"
+    }
+}
 
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-2"
+}
+
+
+resource "aws_internet_gateway" "internet_gateway" {
+    vpc_id = test_vpc.vpc.id // 
+    /*
+     * test_vpc is the vpc we already have
+     * and it has 2 private (10.20.30.0/24, 10.20.31.0/24) and 2 public subnets(10.20.32.0/24, 10.20.33.0/24)
+    */
+}
+
+resource "aws_route_table" "public" {
+    vpc_id = test_vpc.vpc.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.internet_gateway.id
+    }
+}
+
+resource "aws_route_table_association" "route_table_association" {
+    subnet_id      = test_vpc-subnet.pub_subnet.id //assume test_vpc-subnet is the subnet that we already have
+    route_table_id = aws_route_table.public.id
+}
+
+resource "aws_security_group" "ecs_sg" {
+    vpc_id      = test_vpc.vpc.id
+
+    ingress {
+        from_port       = 22
+        to_port         = 22
+        protocol        = "tcp"
+        cidr_blocks     = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port       = 443
+        to_port         = 443
+        protocol        = "tcp"
+        cidr_blocks     = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port       = 0
+        to_port         = 65535
+        protocol        = "tcp"
+        cidr_blocks     = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_security_group" "rds_sg" {
+    vpc_id      = test_vpc.vpc.id
+
+    ingress {
+        protocol        = "tcp"
+        from_port       = 3306
+        to_port         = 3306
+        cidr_blocks     = ["0.0.0.0/0"]
+        security_groups = [aws_security_group.ecs_sg.id]
+    }
+
+    egress {
+        from_port       = 0
+        to_port         = 65535
+        protocol        = "tcp"
+        cidr_blocks     = ["0.0.0.0/0"]
+    }
+}
+
+
+data "aws_iam_policy_document" "ecs_agent" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ecs_agent" {
+  name               = "ecs-agent"
+  assume_role_policy = data.aws_iam_policy_document.ecs_agent.json
+}
+
+
+resource "aws_iam_role_policy_attachment" "ecs_agent" {
+  role       = "aws_iam_role.ecs_agent.name"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
+resource "aws_iam_instance_profile" "ecs_agent" {
+  name = "ecs-agent"
+  role = aws_iam_role.ecs_agent.name
+}
+
+resource "aws_iam_role" "ecs-autoscale-role" {
+  name = "ecs-scale-application"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "application-autoscaling.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "ecs-autoscale" {
+  role = aws_iam_role.ecs-autoscale-role.id
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceAutoscaleRole"
+}
+
+resource "aws_launch_configuration" "ecs_launch_config" {
+    image_id             = "ami-xxxxxxxxxxxx" //Assume we have a launch template already
+    iam_instance_profile = aws_iam_instance_profile.ecs_agent.name
+    security_groups      = [aws_security_group.ecs_sg.id]
+    user_data            = "#!/bin/bash\necho ECS_CLUSTER=test-cluster >> /etc/ecs/ecs.config"
+    instance_type        = "t2.micro"
+}
+
+resource "aws_appautoscaling_target" "ecs_target" {
+  max_capacity       = 2
+  min_capacity       = 1
+  resource_id        = "service/app_clustername/app_servicename"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+  role_arn           = aws_iam_role.ecs-autoscale-role.arn
+}
+
+resource "aws_appautoscaling_policy" "ecs_target_cpu" {
+  name               = "application-scaling-policy-cpu"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.ecs_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+    target_value = 80
+  }
+  depends_on = [aws_appautoscaling_target.ecs_target]
+}
+resource "aws_appautoscaling_policy" "ecs_target_memory" {
+  name               = "application-scaling-policy-memory"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.ecs_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageMemoryUtilization"
+    }
+    target_value = 80
+  }
+  depends_on = [aws_appautoscaling_target.ecs_target]
+}
+
+/*
+resource "aws_autoscaling_group" "failure_analysis_ecs_asg" {
+    name                      = "asg"
+    vpc_zone_identifier       = [aws_subnet.pub_subnet.id]
+    launch_configuration      = aws_launch_configuration.ecs_launch_config.name
+
+    desired_capacity          = 2
+    min_size                  = 1
+    max_size                  = 10
+    health_check_grace_period = 300
+    health_check_type         = "EC2"
+}*/
+
+resource "aws_db_subnet_group" "db_subnet_group" {
+    subnet_ids  = [test_subnet.pub_subnet.id]
+}
+
+resource "aws_db_instance" "mysql" {
+    identifier                = "mysql"
+    allocated_storage         = 5
+    backup_retention_period   = 2
+    backup_window             = "01:00-01:30"
+    maintenance_window        = "sun:03:00-sun:03:30"
+    multi_az                  = true
+    engine                    = "mysql"
+    engine_version            = "5.7"
+    instance_class            = "db.t2.micro"
+    name                      = "worker_db"
+    username                  = "worker"
+    password                  = "worker"
+    port                      = "3306"
+    db_subnet_group_name      = aws_db_subnet_group.db_subnet_group.id
+    vpc_security_group_ids    = [aws_security_group.rds_sg.id, aws_security_group.ecs_sg.id]
+    skip_final_snapshot       = true
+    final_snapshot_identifier = "worker-final"
+    publicly_accessible       = true
+}
+
+resource "aws_ecr_repository" "worker" {
+    name  = "worker"
+}
+
+resource "aws_ecs_cluster" "ecs_cluster" {
+    name  = "test-cluster"
+}
+
+resource "aws_ecs_task_definition" "task_definition" {
+  family                = "worker"
+  container_definitions = data.template_file.task_definition_template.rendered
+}
+
+resource "aws_ecs_service" "worker" {
+  name            = "worker"
+  cluster         = aws_ecs_cluster.ecs_cluster.id
+  task_definition = aws_ecs_task_definition.task_definition.arn
+  desired_count   = 2
+}
+
+resource "aws_acm_certificate" "default" {
+  provider = "aws.acm"
+  domain_name = "test.wowcher.co.uk"
+  validation_method = "DNS"
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_health_check" "ecs_health_check" {
+  fqdn              = "test.wowcher.co.uk"
+  port              = 80
+  type              = "HTTPS"
+  resource_path     = "/"
+  failure_threshold = "5"
+  request_interval  = "30"
+
+}
